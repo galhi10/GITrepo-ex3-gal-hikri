bc6dc98afd8a3b2f1f6449ce34c1e6b5276ee953
Gal Hikri <galhi@mta.ac.il>
2023-04-22 13:04:13 +0300

    another code change

--- GalHikri.java
+++ GalHikri.java
@@ -2,7 +2,7 @@
 public class GalHikri {
     public static void main(String[] args) {
         for(int i =1; i<=10; i++) {
-            System.out.println("Gal Hikri");
+            System.out.println("A different print :)");
         }
     }
 }
